584137a378e7a7386bffc2a7a6aff8d747a761bc
Hakan Albay <hakanalbay2@gmail.com>
2018-11-25 00:18:48 +0300

    Dizi_IV_Hashtable

---
+++ Koleksiyonlar/Dizi-IV-Hashtable/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dizi_IV_Hashtable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ciceksepeti = new Hashtable();
+            //Veri Ekleme
+            ciceksepeti.Add(121341, "kardelen");
+            ciceksepeti.Add("2114mdakak", "zambak");
+            ciceksepeti.Add(7166, "orkide");
+            //Veri Güncelleme
+            ciceksepeti["2114mdakak"] = "Zambak";
+            //Veri Silme
+            ciceksepeti.Remove(7166);
+            //Veri Listeleme 
+            foreach (DictionaryEntry item in ciceksepeti)
+            {
+                Console.WriteLine("Anahtar:{0}-Deger:{1}", item.Key, item.Value);
+            }
+            //arama işlemleri(key yada values)
+            bool keycontrol = ciceksepeti.ContainsKey(7166);
+            bool valuescontrol = ciceksepeti.ContainsValue("zambak");
+        }
+    }
+}
