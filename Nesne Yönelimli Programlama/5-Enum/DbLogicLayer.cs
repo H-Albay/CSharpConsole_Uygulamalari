e697852938d1c411e43d981f1baf8d1125995579
Hakan Albay <hakanalbay2@gmail.com>
2019-01-11 01:09:42 +0300

    Enum Kullanım Amacı Nedir?

---
+++ Nesne Yönelimli Programlama/5-Enum/DbLogicLayer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_Enum
+{
+    public class DbLogicLayer
+    {
+        public DbKayıtİslemleri Kaydet()
+        {
+            /*
+             *  gerekli işlemler
+             */
+            int sonuc = 1;
+            return sonuc > 0 ? DbKayıtİslemleri.Basarılı : DbKayıtİslemleri.Basarısız;
+        }
+
+        public DbKayıtİslemleri Güncelle()
+        {
+            /*
+            *  gerekli işlemler
+            */
+            int sonuc = 0;
+            return sonuc > 0 ? DbKayıtİslemleri.Basarılı : DbKayıtİslemleri.Basarısız;
+        }
+
+        public DbKayıtİslemleri Sil()
+        {
+            /*
+            *  gerekli işlemler
+            */
+            int sonuc = -1;
+            return sonuc < 0 ? DbKayıtİslemleri.Basarılı : DbKayıtİslemleri.HataOlustu;
+
+        }
+
+        public  enum DbKayıtİslemleri
+        {
+            Basarılı=666, Basarısız=18, HataOlustu=23
+        }
+    }
+}
---
+++ Nesne Yönelimli Programlama/5-Enum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DbLogicLayer DLL = new DbLogicLayer();
+
+
+            DbLogicLayer.DbKayıtİslemleri GDeger = DLL.Kaydet();
+
+            switch (GDeger)
+            {   //Enumlar arkaplanda sayısal değerlerle işlem yapmamıza yaramaktadır.
+                //Sayısal değerlerle çalışmak kod yazarken kolaylık sağlamaktadır.
+                //Büyük uygulamalarda işimizi kolaylaştırmaktadır;
+                //çünkü işlemin durumları yazı tipinde kod  tarafında sayısal veriler tutulmaktda
+                case DbLogicLayer.DbKayıtİslemleri.Basarılı:
+                    break;
+                case DbLogicLayer.DbKayıtİslemleri.Basarısız:
+                    break;
+                case DbLogicLayer.DbKayıtİslemleri.HataOlustu:
+                    break;
+            }
+        }
+    }
+}
