9c6ddc060b14e38de3f4fcd6aa43a63772555b10
Hakan Albay <hakanalbay2@gmail.com>
2019-02-09 02:56:07 +0300

    2_Kapsulleme.Uyg_I

---
+++ Nesne Yönelimli Programlama/2-Kapsülleme/2-Kapsulleme.Uyg-I/2-Kapsulleme.Uyg-I/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Kapsulleme.Uyg_I
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ogrenci o1 = new ogrenci();
+            ogrenci o2 = new ogrenci();
+            ogrenci o3 = new ogrenci();
+
+            o1.ogrno = "85236417";  //Doğru giriş
+            o2.ogrno = "741258";    //8 haneden az öğrenci girişi
+            o3.ogrno = "41HS5523";  //8 haneli öğrenci numarası fakat karakterlerin tümü sayısal olmalı
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Nesne Yönelimli Programlama/2-Kapsülleme/2-Kapsulleme.Uyg-I/2-Kapsulleme.Uyg-I/ogrenci.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Kapsulleme.Uyg_I
+{
+    public class ogrenci
+    {
+        private string _ogrno;
+
+        public string ogrno
+        {
+            private get { return _ogrno.Substring(0, 5); }
+            set
+            {
+                if (value.Length == 8)
+                {
+                    bool bayrak = false;
+                    for (int i = 0; i < value.Length; i++)
+                    {
+                        bool karakterkontrol = char.IsNumber(value[i]);
+                        if (karakterkontrol)
+                        {
+                            //öğrenci numarası sayısal girilmiş.
+
+                        }
+                        else
+                        {
+                            bayrak = true;
+                            break;
+                        }
+
+                    }
+                    if (bayrak)
+                    {
+                        Console.WriteLine("Öğrenci numarası içindeki değerler sayısal olmalıdır.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ogrenci Numarası: {0}***", value.Substring(0, 5));
+                        this._ogrno = value;
+                    }
+
+
+                }
+                else { Console.WriteLine("Öğrenci Numarası 8 haneli olmalıdır."); }
+            }
+
+        }
+    }
+}
